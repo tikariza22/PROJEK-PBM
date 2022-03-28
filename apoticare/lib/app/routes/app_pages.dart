d9f559d9f5abf3a513e3ca207968f21fac190345
Fiki Aviantono <56351437+fikia13@users.noreply.github.com>
2022-03-28 20:46:38 +0700

    pembuatan page produk
    halaman tampilan produk produk yang akan ditampilkan pada pengguna

--- apoticare/lib/app/routes/app_pages.dart
+++ apoticare/lib/app/routes/app_pages.dart
@@ -4,6 +4,8 @@ import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 import '../modules/login/bindings/login_binding.dart';
 import '../modules/login/views/login_view.dart';
+import '../modules/product/bindings/product_binding.dart';
+import '../modules/product/views/product_view.dart';
 import '../modules/welcome/bindings/welcome_binding.dart';
 import '../modules/welcome/views/welcome_view.dart';
 
@@ -29,6 +31,11 @@ class AppPages {
       name: _Paths.LOGIN,
       page: () => LoginView(),
       binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.PRODUCT,
+      page: () => ProductView(),
+      binding: ProductBinding(),
     ),
   ];
 }
--- apoticare/lib/app/routes/app_routes.dart
+++ apoticare/lib/app/routes/app_routes.dart
@@ -6,6 +6,7 @@ abstract class Routes {
   static const HOME = _Paths.HOME;
   static const WELCOME = _Paths.WELCOME;
   static const LOGIN = _Paths.LOGIN;
+  static const PRODUCT = _Paths.PRODUCT;
 }
 
 abstract class _Paths {
@@ -13,4 +14,5 @@ abstract class _Paths {
   static const HOME = '/home';
   static const WELCOME = '/welcome';
   static const LOGIN = '/login';
+  static const PRODUCT = '/product';
 }
