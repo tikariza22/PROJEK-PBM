7f93da222207a98790a96ab70c0395f5be9bde5a
Fiki Aviantono <56351437+fikia13@users.noreply.github.com>
2022-04-23 11:56:05 +0700

    update produk

--- lib/app/modules/product/views/product_view.dart
+++ lib/app/modules/product/views/product_view.dart
@@ -7,11 +7,40 @@ import '../controllers/product_controller.dart';
 class ProductView extends GetView<ProductController> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'ProductView is working',
-        style: TextStyle(fontSize: 20),
-      ),
+    return GridView(
+      gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      children: [
+        Container(
+          child: Column(children: [
+            Container(
+              child: Image.asset("assets/examples1.png"),
+            ),
+            Text("Bodrex Extra"),
+            Text("150 ml bose"),
+            Row(
+              children: [
+                Text("Rp. 150.000"),
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.add_circle_rounded,
+                      color: Color(0xff38B0A9),
+                    ))
+              ],
+            )
+          ]),
+        ),
+        Container(
+          color: Colors.amber,
+        ),
+        Container(
+          color: Colors.amber,
+        ),
+        Container(
+          color: Colors.amber,
+        ),
+      ],
     );
   }
 }
